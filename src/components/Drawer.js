b3d9e251350f836470fc1036b57ff91265713b08
Kosianchuk Dmitriy <kosianchuk.d@gmail.com>
2018-01-19 15:58:55 +0200

    add icons

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import MenuItem from "material-ui/MenuItem";
-import Drawer from "material-ui/Drawer";
+import React from 'react'
+import MenuItem from "material-ui/MenuItem"
+import Drawer from "material-ui/Drawer"
+import ActionHome from 'material-ui/svg-icons/action/home'
+import ContactIcon from 'material-ui/svg-icons/communication/contacts';
+import { pink500 } from 'material-ui/styles/colors'
+import { purple600 } from 'material-ui/styles/colors'
+
+
 export const AppDrawer = (props) => {
   return (
     <Drawer 
@@ -9,8 +15,24 @@ export const AppDrawer = (props) => {
       onRequestChange={open => props.onToggle(open)}
     >
 
-    <MenuItem>Главное</MenuItem>
-    <MenuItem>Контакты</MenuItem>
+    <MenuItem 
+      leftIcon={
+        <ActionHome
+          color={pink500}
+          hoverColor={purple600}
+        />
+      }
+    >
+      Главное
+    </MenuItem>
+    <MenuItem
+      leftIcon={
+        <ContactIcon 
+          color={pink500}
+          hoverColor={purple600}
+        />
+      }
+      >Контакты</MenuItem>
   </Drawer>
   )
 }
